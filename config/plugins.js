251b115181b55aad3054add9202f2c3d33538b6e
Your Name <>
2023-07-06 18:32:43 +0200

    fixed graphql count

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,13 @@
 module.exports = {
   'drag-drop-content-types': {
     enabled: true
-  }
+  },
+  graphql: {
+    enabled: true,
+    config: {
+      defaultLimit: 100,
+      depthLimit: 100,
+      maxLimit: 100,
+    }
+  },
 }
