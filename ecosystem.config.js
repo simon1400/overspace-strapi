0691513d77b3a1790adb6bc34f36afdff99b79cd
Your Name <>
2023-07-17 13:01:24 +0200

    downgrade image sharp

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name   : "Overspace strapi",
-    script : "yarn start",
+    script : "npm start",
     env_production: {}
   }],
 
@@ -12,7 +12,7 @@ module.exports = {
       ref  : 'origin/main',
       repo : 'git@github.com:simon1400/overspace-strapi.git',
       path : '/var/www/overspace/strapi',
-      'post-deploy' : 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
     }
   }
 };
