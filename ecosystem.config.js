94c5423a1d8f2c1596da2802d9e8cebf41158942
Dmytro Pechunka <pechunka11@gmail.com>
2025-11-21 22:35:38 +0100

    fixed pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,44 @@
 module.exports = {
-  apps : [{
-    name   : "Overspace strapi",
-    script : "npm start",
-    env_production: {}
-  }],
+  apps: [
+    {
+      name: 'overspace-strapi',
+      script: './node_modules/@strapi/strapi/bin/strapi.js',
+      args: 'start',
+      cwd: '/var/www/overspace/strapi/current',
+      instances: 1,
+      exec_mode: 'fork',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production',
+        HOST: '0.0.0.0',
+        PORT: 1334,
+      },
+      error_file: './logs/pm2-error.log',
+      out_file: './logs/pm2-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      merge_logs: true,
+      time: true,
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'dimi',
-      host : ['89.221.216.23'],
-      ref  : 'origin/main',
-      repo : 'git@github.com:simon1400/overspace-strapi.git',
-      path : '/var/www/overspace/strapi',
-      'post-deploy' : 'npm i && npm run build && pm2 reload ecosystem.config.js --env production',
-    }
-  }
+  deploy: {
+    production: {
+      user: 'dimi',
+      host: ['89.221.216.23'],
+      ref: 'origin/main',
+      repo: 'git@github.com:simon1400/overspace-strapi.git',
+      path: '/var/www/overspace/strapi',
+      ssh_options: 'StrictHostKeyChecking=no',
+      'pre-deploy-local': 'echo "Deploying to production..."',
+      'post-deploy':
+        'npm ci && ' +
+        'npm run build && ' +
+        'mkdir -p logs && ' +
+        'pm2 reload ecosystem.config.js --env production && ' +
+        'pm2 save',
+      'pre-setup': 'mkdir -p /var/www/overspace/strapi',
+    },
+  },
 };
